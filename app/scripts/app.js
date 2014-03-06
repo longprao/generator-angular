843d30e8ca4445bad1e2f66dfdaf157865f7feac
Long Doan <longprao@gmail.com>
2014-03-06 14:50:31 +0700

    add ui router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,59 @@
 'use strict';
 
-angular.module('generatorApp', [
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('generatorApp', ['ui.router', 'ui.bootstrap']).
+    config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
+
+        $locationProvider.html5Mode(true).hashPrefix('!');
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.
+            state('base', {
+                url: '/',
+                abstract: true,
+                views: {
+                    'container': {
+                        templateUrl: 'views/index.html'
+                    }
+                }
+            })
+
+        //Add trailing slash
+        $urlRouterProvider.rule(function($injector, $location) {
+            var path = $location.path(),
+            // Note: misnomer. This returns a query object, not a search string
+                search = $location.search(),
+                params;
+
+            // check to see if the path already ends in '/'
+            if (path[path.length - 1] === '/') {
+                return path;
+            }
+
+            // If there was no search string / query params, return with a `/`
+            if (Object.keys(search).length === 0) {
+                return path + '/';
+            }
+
+            // Otherwise build the search string and return a `/?` prefix
+            params = [];
+            angular.forEach(search, function(v, k){
+                params.push(k + '=' + v);
+            });
+            return path + '/?' + params.join('&');
+        });
+    }).
+    directive('loadingContainer', function () {
+        return {
+            restrict: 'A',
+            scope: false,
+            link: function(scope, element, attrs) {
+                var loadingLayer = angular.element('<div class="loading"></div>');
+                element.append(loadingLayer);
+                element.addClass('loading-container');
+                scope.$watch(attrs.loadingContainer, function(value) {
+                    loadingLayer.toggleClass('ng-hide', !value);
+                });
+            }
+        };
+    });
